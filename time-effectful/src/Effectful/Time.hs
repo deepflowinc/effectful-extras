77194955f810947d7fd5c935ffd4709bb046d58b
Hiromi ISHII <hiromi.ishii@deepflow.co.jp>
2024-03-05 10:52:04 +0900

    fix: fourmolu

--- time-effectful/src/Effectful/Time.hs
+++ time-effectful/src/Effectful/Time.hs
@@ -25,14 +25,14 @@ type instance DispatchOf Clock = 'Static 'WithSideEffects
 
 data instance StaticRep Clock = TimeRep
 
-getCurrentTimeZone :: Clock :> es => Eff es TimeZone
+getCurrentTimeZone :: (Clock :> es) => Eff es TimeZone
 getCurrentTimeZone = unsafeEff_ Time.getCurrentTimeZone
 
-getCurrentTime :: Clock :> es => Eff es UTCTime
+getCurrentTime :: (Clock :> es) => Eff es UTCTime
 getCurrentTime = unsafeEff_ Time.getCurrentTime
 
-getZonedTime :: Clock :> es => Eff es ZonedTime
+getZonedTime :: (Clock :> es) => Eff es ZonedTime
 getZonedTime = unsafeEff_ Time.getZonedTime
 
-runClock :: IOE :> es => Eff (Clock ': es) a -> Eff es a
+runClock :: (IOE :> es) => Eff (Clock ': es) a -> Eff es a
 runClock = evalStaticRep TimeRep
