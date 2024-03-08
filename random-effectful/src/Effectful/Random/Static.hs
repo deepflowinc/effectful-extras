ca2e137e7b50e17bcd5756ac7cfd46549f23354c
Hiromi ISHII <hiromi.ishii@deepflow.co.jp>
2024-03-08 13:22:31 +0900

    feat(random-effectful): other combinators

--- random-effectful/src/Effectful/Random/Static.hs
+++ random-effectful/src/Effectful/Random/Static.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
@@ -9,13 +8,37 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoFieldSelectors #-}
 
-module Effectful.Random.Static (Random, evalRandom, runRandom, uniformR) where
+module Effectful.Random.Static (
+  Random,
+  evalRandom,
+  runRandom,
+  split,
 
+  -- * Combinators
+  Rand.Uniform,
+  uniform,
+  Rand.UniformRange,
+  uniformR,
+  genByteString,
+
+  -- ** Additional utilities
+  element,
+
+  -- * Re-exports
+  RandomGen (),
+  StdGen,
+  mkStdGen,
+) where
+
+import qualified Data.ByteString as BS
 import Data.Coerce (coerce)
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NE
+import qualified Data.Vector as V
 import Effectful
 import Effectful.Concurrent.MVar.Strict (Concurrent, MVar, modifyMVar, newMVar, readMVar)
 import Effectful.Dispatch.Static
-import System.Random (StdGen)
+import System.Random (RandomGen (), StdGen, mkStdGen)
 import qualified System.Random as Rand
 
 data Random :: Effect
@@ -38,6 +61,19 @@ uniformR ran = do
     let (a, g') = Rand.uniformR ran g
     pure (g', a)
 
+uniform ::
+  forall es a.
+  ( Random :> es
+  , Rand.Uniform a
+  , Concurrent :> es
+  ) =>
+  Eff es a
+uniform = do
+  Random mg <- getStaticRep @Random
+  modifyMVar mg $ \g -> do
+    let (a, g') = Rand.uniform g
+    pure (g', a)
+
 runRandom :: forall es a. (Concurrent :> es) => StdGen -> Eff (Random ': es) a -> Eff es (a, StdGen)
 {-# INLINE runRandom #-}
 runRandom g act = do
@@ -45,8 +81,33 @@ runRandom g act = do
   mapM (readMVar . coerce)
     =<< runStaticRep @Random @_ @es @a rep act
 
+-- | Splits the current standard generator, updates it with one of the results,
+split :: (Random :> es, Concurrent :> es) => Eff es StdGen
+{-# INLINE split #-}
+split = do
+  Random mg <- getStaticRep @Random
+  modifyMVar mg $ \g -> do
+    let (g', g'') = Rand.split g
+    pure (g'', g')
+
+genByteString ::
+  (Random :> es, Concurrent :> es) =>
+  Int ->
+  Eff es BS.ByteString
+genByteString n = do
+  Random mg <- getStaticRep @Random
+  modifyMVar mg $ \g ->
+    let (bs, g') = Rand.genByteString n g
+     in pure (g', bs)
+
 evalRandom :: (Concurrent :> es) => StdGen -> Eff (Random ': es) a -> Eff es a
 {-# INLINE evalRandom #-}
 evalRandom g act = do
   mg <- Random <$> newMVar g
   evalStaticRep mg act
+
+element ::
+  (Random :> es, Concurrent :> es) => NonEmpty a -> Eff es a
+element els = do
+  let v = V.fromList $ NE.toList els
+  (v V.!) <$> uniformR (0, V.length v - 1)
