42dab068dab0116372cb6edad35c2b3431e343d1
Hiromi ISHII <hiromi.ishii@deepflow.co.jp>
2024-03-08 13:55:48 +0900

    feat(random-effectful): compat with stateful interface of `random`

--- random-effectful/src/Effectful/Random/Static.hs
+++ random-effectful/src/Effectful/Random/Static.hs
@@ -1,12 +1,18 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoFieldSelectors #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
 module Effectful.Random.Static (
   Random,
@@ -14,15 +20,25 @@ module Effectful.Random.Static (
   runRandom,
   split,
 
+  -- ** Initialisation with global StdGen
+  getStdGen,
+  newStdGen,
+
   -- * Combinators
   Rand.Uniform,
   uniform,
   Rand.UniformRange,
   uniformR,
   genByteString,
+  genShortByteString,
+  withStdGen,
 
   -- ** Additional utilities
   element,
+
+  -- ** Compat with 'Rand.StatefulGen'
+  EffGen (..),
+  FrozenEffGen (..),
 
   -- * Re-exports
   RandomGen (),
@@ -31,15 +47,18 @@ module Effectful.Random.Static (
 ) where
 
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Short as SBS
 import Data.Coerce (coerce)
 import Data.List.NonEmpty (NonEmpty)
 import qualified Data.List.NonEmpty as NE
 import qualified Data.Vector as V
 import Effectful
-import Effectful.Concurrent.MVar.Strict (Concurrent, MVar, modifyMVar, newMVar, readMVar)
+import Effectful.Concurrent.MVar.Strict (Concurrent, MVar, modifyMVar, newMVar, readMVar, swapMVar)
 import Effectful.Dispatch.Static
+import GHC.Generics (Generic)
 import System.Random (RandomGen (), StdGen, mkStdGen)
 import qualified System.Random as Rand
+import qualified System.Random.Stateful as RS
 
 data Random :: Effect
 
@@ -100,6 +119,17 @@ genByteString n = do
     let (bs, g') = Rand.genByteString n g
      in pure (g', bs)
 
+genShortByteString ::
+  (Random :> es, Concurrent :> es) =>
+  Int ->
+  Eff es SBS.ShortByteString
+{-# INLINE genShortByteString #-}
+genShortByteString n = do
+  Random mg <- getStaticRep @Random
+  modifyMVar mg $ \g ->
+    let (bs, g') = Rand.genShortByteString n g
+     in pure (g', bs)
+
 evalRandom :: (Concurrent :> es) => StdGen -> Eff (Random ': es) a -> Eff es a
 {-# INLINE evalRandom #-}
 evalRandom g act = do
@@ -111,3 +141,61 @@ element ::
 element els = do
   let v = V.fromList $ NE.toList els
   (v V.!) <$> uniformR (0, V.length v - 1)
+
+getStdGen :: (MonadIO m) => m StdGen
+getStdGen = liftIO Rand.getStdGen
+
+newStdGen :: (MonadIO m) => m StdGen
+newStdGen = liftIO Rand.newStdGen
+
+data EffGen = EffGen
+  deriving (Show, Eq, Ord, Generic)
+
+withStdGen ::
+  (Concurrent :> es, Random :> es) =>
+  (StdGen -> (a, StdGen)) ->
+  Eff es a
+withStdGen f = do
+  Random mg <- getStaticRep @Random
+  modifyMVar mg $ \g ->
+    let (a, g') = f g
+     in pure (g', a)
+
+instance (Concurrent :> es, Random :> es) => RS.StatefulGen EffGen (Eff es) where
+  uniformWord32R = const . withStdGen . Rand.genWord32R
+  {-# INLINE uniformWord32R #-}
+  uniformWord64R = const . withStdGen . Rand.genWord64R
+  {-# INLINE uniformWord64R #-}
+  uniformWord8 = const $ withStdGen Rand.genWord8
+  {-# INLINE uniformWord8 #-}
+  uniformWord16 = const $ withStdGen Rand.genWord16
+  {-# INLINE uniformWord16 #-}
+  uniformWord32 = const $ withStdGen Rand.genWord32
+  {-# INLINE uniformWord32 #-}
+  uniformWord64 = const $ withStdGen Rand.genWord64
+  {-# INLINE uniformWord64 #-}
+  uniformShortByteString = const . genShortByteString
+  {-# INLINE uniformShortByteString #-}
+
+newtype FrozenEffGen = FrozenEffGen {getFrozenEffGen :: StdGen}
+  deriving (Show, Eq, Generic)
+  deriving newtype (RandomGen)
+
+instance (Concurrent :> es, Random :> es) => RS.FrozenGen FrozenEffGen (Eff es) where
+  type MutableGen FrozenEffGen (Eff es) = EffGen
+  freezeGen = const $ do
+    Random mg <- getStaticRep @Random
+    FrozenEffGen <$> readMVar mg
+  {-# INLINE freezeGen #-}
+  thawGen (FrozenEffGen g) = do
+    Random mg <- getStaticRep @Random
+    EffGen <$ swapMVar mg g
+  {-# INLINE thawGen #-}
+
+instance
+  (Concurrent :> es, Random :> es) =>
+  RS.RandomGenM EffGen FrozenEffGen (Eff es)
+  where
+  applyRandomGenM :: forall a. (FrozenEffGen -> (a, FrozenEffGen)) -> EffGen -> Eff es a
+  applyRandomGenM = const . coerce (withStdGen @es @a)
+  {-# INLINE applyRandomGenM #-}
